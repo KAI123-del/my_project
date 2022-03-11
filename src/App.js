14dc43f7d7ca36e70ff6f3dea6cc4871daad0a1e
kai123-del <bhattkailash786@gmail.com>
2022-03-11 16:07:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-6xl bg-red-400 min-h-screen ">
+      <div>
+        Hello world!!!!!
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
